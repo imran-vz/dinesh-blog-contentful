bbf589632db53b6039937a593a71fa7844c4f1d4
Mohammed Imran <mohammedimran86992@gmail.com>
2025-06-07 21:27:39 +0530

    remove unused files and clean up import statements in Article components

--- src/components/features/article/ArticleAuthor.tsx
+++ src/components/features/article/ArticleAuthor.tsx
@@ -1,42 +1,47 @@
-'use client';
+"use client";
 
 import {
-  useContentfulInspectorMode,
-  useContentfulLiveUpdates,
-} from '@contentful/live-preview/react';
+	useContentfulInspectorMode,
+	useContentfulLiveUpdates,
+} from "@contentful/live-preview/react";
 
-import { CtfImage } from '@src/components/features/contentful';
-import { PageBlogPostFieldsFragment } from '@src/lib/__generated/sdk';
+import { CtfImage } from "@src/components/features/contentful";
+import type { PageBlogPostFieldsFragment } from "@src/lib/__generated/sdk";
 
 interface ArticleAuthorProps {
-  article: PageBlogPostFieldsFragment;
+	article: PageBlogPostFieldsFragment;
 }
 
 export const ArticleAuthor = ({ article }: ArticleAuthorProps) => {
-  const { author } = useContentfulLiveUpdates(article);
-  const inspectorProps = useContentfulInspectorMode({ entryId: author?.sys.id });
+	const { author } = useContentfulLiveUpdates(article);
+	const inspectorProps = useContentfulInspectorMode({
+		entryId: author?.sys.id,
+	});
 
-  return (
-    <div className="flex items-center">
-      <div
-        className="mr-2 overflow-hidden rounded-full border border-blue500"
-        {...inspectorProps({ fieldId: 'avatar' })}
-      >
-        {author?.avatar && (
-          <CtfImage
-            nextImageProps={{
-              width: 28,
-              height: 28,
-              sizes: undefined,
-              placeholder: undefined,
-            }}
-            {...author.avatar}
-          />
-        )}
-      </div>
-      <span className="text-xs leading-none text-gray600" {...inspectorProps({ fieldId: 'name' })}>
-        {author?.name}
-      </span>
-    </div>
-  );
+	return (
+		<div className="flex items-center">
+			<div
+				className="mr-2 overflow-hidden rounded-full border border-blue500"
+				{...inspectorProps({ fieldId: "avatar" })}
+			>
+				{author?.avatar && (
+					<CtfImage
+						nextImageProps={{
+							width: 28,
+							height: 28,
+							sizes: undefined,
+							placeholder: undefined,
+						}}
+						{...author.avatar}
+					/>
+				)}
+			</div>
+			<span
+				className="text-xs leading-none text-gray600"
+				{...inspectorProps({ fieldId: "name" })}
+			>
+				{author?.name}
+			</span>
+		</div>
+	);
 };
